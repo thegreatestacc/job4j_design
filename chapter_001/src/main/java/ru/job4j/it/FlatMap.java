cbb31807000cf65abcb295b88aa239b9763bf6d0
thegreatestacc <thegreatestemailname@gmail.com>
2020-10-16 14:57:04 +0300

    flat map

---
+++ chapter_001/src/main/java/ru/job4j/it/FlatMap.java
@@ -0,0 +1,47 @@
+package ru.job4j.it;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+public class FlatMap<T> implements Iterator<T> {
+
+    private final Iterator<Iterator<T>> data;
+    private Iterator<T> cursor;
+
+    public FlatMap(Iterator<Iterator<T>> data) {
+        this.data = data;
+    }
+
+    @Override
+    public boolean hasNext() {
+        if (cursor == null && data.hasNext()) {
+            cursor = data.next();
+        }
+        return cursor != null;
+    }
+
+    @Override
+    public T next() {
+        if (!hasNext()) {
+            throw new NoSuchElementException();
+        }
+        T element = cursor.next();
+        if (!cursor.hasNext()) {
+            cursor = null;
+        }
+        return element;
+    }
+
+    public static void main(String[] args) {
+        Iterator<Iterator<Integer>> data = List.of(
+                List.of(1, 2, 3).iterator(),
+                List.of(4, 5, 6).iterator(),
+                List.of(7, 8, 9).iterator()
+        ).iterator();
+        FlatMap<Integer> flat = new FlatMap<>(data);
+        while (flat.hasNext()) {
+            System.out.println(flat.next());
+        }
+    }
+}
