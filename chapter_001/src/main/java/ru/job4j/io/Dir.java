f507725153fb077cef79cad5ce5a8d9ccb449ee1
thegreatestacc <thegreatestemailname@gmail.com>
2020-11-11 21:57:39 +0300

    files size

---
+++ chapter_001/src/main/java/ru/job4j/io/Dir.java
@@ -0,0 +1,19 @@
+package ru.job4j.io;
+
+import java.io.File;
+
+public class Dir {
+    public static void main(String[] args) {
+        File file = new File("C:\\Users\\ARENA\\Desktop\\job4j\\job4j_design");
+        if (!file.exists()) {
+            throw new IllegalArgumentException(String.format("Not exist %s", file.getAbsoluteFile()));
+        }
+        if (!file.isDirectory()) {
+            throw new IllegalArgumentException(String.format("Not directory %s", file.getAbsoluteFile()));
+        }
+        System.out.println(String.format("size : %s", file.getTotalSpace()));
+        for (File subfile : file.listFiles()) {
+            System.out.println(subfile.getAbsoluteFile().getName() + ", size: " + subfile.length());
+        }
+    }
+}
