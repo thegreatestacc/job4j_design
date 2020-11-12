b3365ab87ce389b73288c3d7de590bea299de1e7
thegreatestacc <thegreatestemailname@gmail.com>
2020-11-12 21:38:12 +0300

    Args Name

---
+++ chapter_001/src/main/java/ru/job4j/io/ArgsName.java
@@ -0,0 +1,36 @@
+package ru.job4j.io;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ArgsName {
+    private final Map<String, String> values = new HashMap<>();
+
+    public String get(String key) {
+        return values.get(key);
+    }
+
+    private void parse(String[] args) {
+        for (String str : args) {
+            String[] split = str.split("=");
+            StringBuilder sb = new StringBuilder(split[0]);
+            sb.deleteCharAt(0);
+            if (split.length > 1) {
+                values.put(sb.toString(), split[1]);
+            } else values.put(split[0], "");
+        }
+    }
+
+    public static ArgsName of(String[] args) {
+        ArgsName names = new ArgsName();
+        names.parse(args);
+        return names;
+    }
+
+    public static void main(String[] args) {
+        ArgsName jvm = ArgsName.of(new String[]{"-Xmx=512", "-encoding=UTF-8"});
+        System.out.println(jvm.get("Xmx"));
+        ArgsName zip = ArgsName.of(new String[]{"-out=project.zip", "-encoding=UTF-8"});
+        System.out.println(zip.get("out"));
+    }
+}
