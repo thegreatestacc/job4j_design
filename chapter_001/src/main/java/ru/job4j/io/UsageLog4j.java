664ba5cd88101846ad426b1c9164b91497d70876
thegreatestacc <thegreatestemailname@gmail.com>
2020-11-18 19:28:41 +0300

    slf4j variables

--- chapter_001/src/main/java/ru/job4j/io/UsageLog4j.java
+++ chapter_001/src/main/java/ru/job4j/io/UsageLog4j.java
@@ -7,10 +7,17 @@ public class UsageLog4j {
     private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
-        LOG.trace("trace message");
-        LOG.debug("debug message");
-        LOG.info("info message");
-        LOG.warn("warn message");
-        LOG.error("error message");
+        String name = "Petr Arsentev";
+        int age = 33;
+        long l = 40;
+        short s = 1;
+        char c = 'c';
+        boolean b = true;
+        byte bt = 2;
+        float fl = 4;
+        double dble = 1.2;
+
+
+        LOG.debug("User info name : {}, age : {}, long : {}, short : {}, char : {}, boolean : {}, byte : {}, float : {}, double : {}", name, age, l, s, c, b, bt, fl, dble);
     }
 }
