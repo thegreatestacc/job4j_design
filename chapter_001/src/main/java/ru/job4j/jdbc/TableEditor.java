273456592c7ae050b460ae1f4febc60e3b53bc3d
thegreatestacc <thegreatestemailname@gmail.com>
2020-12-02 17:13:45 +0300

    table editor changes

--- chapter_001/src/main/java/ru/job4j/jdbc/TableEditor.java
+++ chapter_001/src/main/java/ru/job4j/jdbc/TableEditor.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 import java.sql.*;
 import java.util.*;
 
-public class TableEditor implements AutoCloseable{
+public class TableEditor implements AutoCloseable {
     private Connection connection;
 
     private Properties properties;
@@ -17,76 +17,70 @@ public class TableEditor implements AutoCloseable{
     }
 
     private void initConnection() {
-        connection = null;
+        try {
+            connection = getConnection();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public void createTable(String tableName) {
-        try (var connection = getConnection()) {
-            try (var statement = connection.createStatement()) {
-                var sql = String.format(
-                        "create table if not exist %s (%s, %s);",
-                        tableName,
-                        "id serial primary key",
-                        "name varchar"
-                );
-                statement.execute(sql);
-            }
+        try (var statement = connection.createStatement()) {
+            var sql = String.format(
+                    "create table if not exist %s (%s, %s);",
+                    tableName,
+                    "id serial primary key",
+                    "name varchar"
+            );
+            statement.execute(sql);
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
     public void dropTable(String table) {
-        try (var connection = getConnection()) {
-            try (var statement = connection.createStatement()) {
-                var sql = String.format(
-                        "drop table %s",
-                        table
-                );
-                statement.execute(sql);
-            }
+        try (var statement = connection.createStatement()) {
+            var sql = String.format(
+                    "drop table %s",
+                    table
+            );
+            statement.execute(sql);
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
     public void addColumn(String tableName, String columnName, String type) {
-        try (var connection = getConnection()) {
-            try (var statement = connection.createStatement()) {
-                var sql = String.format(
-                        "alter table %s add %s, %s",
-                        tableName, columnName, type
-                );
-                statement.execute(sql);
-            }
+        try (var statement = connection.createStatement()) {
+            var sql = String.format(
+                    "alter table %s add %s, %s",
+                    tableName, columnName, type
+            );
+            statement.execute(sql);
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
     public void dropColumn(String tableName, String columnName) {
-        try (var connection = getConnection()) {
-            try (var statement = connection.createStatement()) {
-                var sql = String.format(
-                        "alter table %s drop column %s",
-                        tableName, columnName
-                );
-                statement.execute(sql);
-            }
+        try (var statement = connection.createStatement()) {
+            var sql = String.format(
+                    "alter table %s drop column %s",
+                    tableName, columnName
+            );
+            statement.execute(sql);
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
     public void renameColumn(String tableName, String columnName, String newColumnName) {
-        try (var connection = getConnection()) {
-            try (var statement = connection.createStatement()) {
-                var sql = String.format(
-                        "alter table %s rename column %s to %s",
-                        tableName, columnName, newColumnName
-                );
-                statement.execute(sql);
-            }
+        try (var statement = connection.createStatement()) {
+            var sql = String.format(
+                    "alter table %s rename column %s to %s",
+                    tableName, columnName, newColumnName
+            );
+            statement.execute(sql);
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -106,19 +100,20 @@ public class TableEditor implements AutoCloseable{
         return scheme.toString();
     }
 
-    private static Connection getConnection() throws Exception {
+    private Connection getConnection() throws Exception {
         var applicationProperties = getApplicationProperties();
+        var url = applicationProperties.getProperty("url");
+        properties.setProperty("login", applicationProperties.getProperty("login"));
+        properties.setProperty("password", applicationProperties.getProperty("password"));
         Class.forName("org.postgresql.Driver");
-        var url = applicationProperties.get("url");
-        var login = applicationProperties.get("login");
-        var password = applicationProperties.get("password");
-        return DriverManager.getConnection(url, login, password);
+        return DriverManager.getConnection(url, properties);
     }
 
-    public static Map<String, String> getApplicationProperties() {
+    public static Properties getApplicationProperties() {
         Map<String, String> result = new HashMap<>();
         List<String> list = new ArrayList<>();
-        var path = "C:\\Users\\ARENA\\Desktop\\job4j\\job4j_design\\chapter_001\\src\\main\\resources\\app.properties";
+        Properties properties = new Properties();
+        var path = "chapter_001/src/main/resources/app.properties";
         try (var br = new BufferedReader(new FileReader(path))) {
             br.lines().forEach(list::add);
             for (String str : list) {
@@ -130,7 +125,10 @@ public class TableEditor implements AutoCloseable{
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return result;
+        properties.setProperty("url", result.get("url"));
+        properties.setProperty("login", result.get("login"));
+        properties.setProperty("password", result.get("password"));
+        return properties;
     }
 
     @Override
